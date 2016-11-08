f8b2d70d6766a1c0ccb879e1de885bc7594d2527
Mohsan Saleem <mohsansaleem@gmail.com>
2016-11-08 12:29:02 +0500

    Custom Patcher Interface

---
+++ src/Assets/Scripts/Patcher/Editor/Game Utils.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace TOZ.Utilities
+{
+    public static class GameUtils
+    {
+        [MenuItem("Tools/OpenScene/Custom Patcher")]
+        public static void OpenGameLoginScene()
+        {
+            OpenScene("Custom Patcher");
+        }
+
+        [MenuItem("Tools/Delete Prefs")]
+        public static void DeletePrefs()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+
+        static void OpenScene(string name)
+        {
+            if (EditorApplication.SaveCurrentSceneIfUserWantsTo())
+            {
+                EditorApplication.OpenScene("Assets/Scenes/" + name + ".unity");
+            }
+        }
+    }
+}
