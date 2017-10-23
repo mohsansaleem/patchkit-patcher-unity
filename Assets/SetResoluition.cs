0f7dfd94aa3f7696a437e199e0a815428eb4c4d4
Mohsan Saleem <mohsansaleem@gmail.com>
2017-10-23 15:50:42 +0500

    Changes

---
+++ Assets/SetResoluition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResoluition : MonoBehaviour {
+
+    void Awake()
+    {
+        Screen.SetResolution(1280, 720, false);
+    }
+}
